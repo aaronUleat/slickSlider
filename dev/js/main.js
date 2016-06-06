7a0c2e6266e8bf48081dd7b6d49ea5cb10b6373e
Aaron Ulate <aulate@mybookie.cr>
2016-06-06 13:38:22 -0600

    Simple Slick Slider

---
+++ dev/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+	$('.slick-slider-main').slick({
+		infinite: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows : true,
+		dots : true,
+		autoplay : false,
+		fade: true,
+		speed: 700,
+		pauseOnHover : false
+	});
+});
